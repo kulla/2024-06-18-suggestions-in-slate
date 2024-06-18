54004f13b8c9bc27669243d8b4126a61f503b079
Stephan Kulla <git.mail@kulla.me>
2024-06-18 22:50:48 +0200

    feat: Add basic slate text editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState, useCallback } from "react";
+import { createEditor, Editor, Transforms, Text, BaseEditor } from "slate";
+import { Slate, Editable, withReact, ReactEditor } from "slate-react";
+import { withHistory, HistoryEditor } from "slate-history";
+import "./App.css";
 
-function App() {
+interface CustomText {
+  text: string;
+  bold?: boolean;
+  italic?: boolean;
+}
+
+type CustomElement = { type: "paragraph"; children: CustomText[] };
+
+declare module "slate" {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor & HistoryEditor;
+    Element: CustomElement;
+    Text: CustomText;
+  }
+}
+
+const initialValue: CustomElement[] = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+];
+
+const App = () => {
+  const [value, setValue] = useState<CustomElement[]>(initialValue);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <button onClick={() => toggleMark(editor, "bold")}>Bold</button>
+        <button onClick={() => toggleMark(editor, "italic")}>Italic</button>
+      </div>
+      <Slate
+        editor={editor}
+        initialValue={value}
+        onChange={(newValue) => setValue(newValue as CustomElement[])}
+      >
+        <Editable renderLeaf={(props) => <Leaf {...props} />} />
+      </Slate>
+    </>
   );
+};
+
+function toggleMark(editor: Editor, format: "bold" | "italic") {
+  const isActive = isMarkActive(editor, format);
+  Transforms.setNodes(
+    editor,
+    { [format]: isActive ? null : true },
+    { match: Text.isText, split: true },
+  );
+}
+
+function isMarkActive(editor: Editor, format: "bold" | "italic") {
+  const marks = Editor.marks(editor) || {};
+  return marks[format] === true;
+}
+
+function Leaf(props: { attributes: any; children: any; leaf: CustomText }) {
+  let { children, leaf, attributes } = props;
+  if (leaf.bold) {
+    children = <strong {...attributes}>{children}</strong>;
+  }
+  if (leaf.italic) {
+    children = <em {...attributes}>{children}</em>;
+  }
+  return <span {...props.attributes}>{children}</span>;
 }
 
 export default App;
